265fb14e9cb8c7e55bfe250dd1828c433e7b09e0
potapovDim <dmytrop@weblium.com>
2017-02-08 20:12:32 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<div>
-      <h2>{{title}}</h2>
-        <main-container></main-container>
-      <h1>Hello {{name}}</h1>
-    </div>`,
+  templateUrl: 'app.template.html' ,
 })
 export class AppComponent {
   name = 'Angular';
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+
+const _root = path.resolve(__dirname, '..');
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [_root].concat(args));
+}
 module.exports = {
-  cache: true,
-  devtool: 'source-map',
   entry: {
-   vendor: './src/vendor',
-   polyfills: './src/polyfills',
-   main: './src/main'
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
   },
   output: {
     path: path.join(__dirname,''),
@@ -15,21 +21,54 @@ module.exports = {
     sourceMapFilename: '[name].map',
     chunkFilename: '[id].chunk.js',
   },
-  devServer: {
-    port: 5050,
-    historyApiFallback: true
+  resolve: {
+    extensions: ['.ts', '.js']
   },
   module: {
-    loaders: [
-      {test: /\.ts$/, loader: 'awesome-typescript-loader'},
-      {test: /\.json$/, loader: 'json-loader'},
-      {test: /\.html$/, loader: 'raw-loader'}
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [{
+          loader: 'awesome-typescript-loader',
+          options: {
+            configFileName: 'tsconfig.json'
+          }
+        }, 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: root('src', 'app'),
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader?sourceMap'
+        })
+      },
+      {
+        test: /\.css$/,
+        include: root('src', 'app'),
+        loader: 'raw-loader'
+      }
     ]
   },
-  resolve: {
-    extensions: ['', '.js', '.ts', '.json']
-  },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({name:['polyfills']})
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      root('./src'),
+      {} 
+    ),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
   ]
 }
